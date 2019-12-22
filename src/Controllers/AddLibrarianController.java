a2ffc5cb068092f3d695b476244bf538f7434cca
bunyod99 <khakimov.bunyod99@gmail.com>
2019-12-22 09:53:13 +0500

    Update 22.12.19 9:52

---
+++ src/Controllers/AddLibrarianController.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AddLibrarianController {
+    @FXML
+    public Button addButton;
+    @FXML
+    public TextField firstNameTextField;
+    @FXML
+    public TextField lastNameTextField;
+    @FXML
+    public TextField loginTextField;
+    @FXML
+    public PasswordField passwordPasswordField;
+
+    @FXML
+    public void add(){
+
+    }
+
+}
---
+++ src/Controllers/AddStudentController.java
@@ -0,0 +1,26 @@
+package Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AddStudentController {
+    @FXML
+    public Button addButton;
+    @FXML
+    public TextField firstNameTextField;
+    @FXML
+    public TextField lastNameTextField;
+    @FXML
+    public TextField loginTextField;
+    @FXML
+    public PasswordField passwordPasswordField;
+
+
+    @FXML
+    public void add(){
+
+    }
+
+}
